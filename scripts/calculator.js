ea459f5f0fa209b66278ab862d456c7cbfa54c27
Kat <142260481+Katlego-art@users.noreply.github.com>
2024-12-28 18:56:35 +0200

    displaying numbers on the input

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,18 @@
+let displayNum = document.querySelector(".js-input");
+document.querySelectorAll(".js-btnNum").forEach((button) => {
+  button.addEventListener("click", () => {
+    let calNumber = button.innerHTML;
+
+    displayNum.value += calNumber;
+  });
+});
+
+document.querySelectorAll(".js-mathbtn").forEach((button) => {
+  button.addEventListener("click", () => {
+    let mathOp = button.innerHTML;
+
+    displayNum.value += mathOp;
 
 
-
-
-
-
-
+  });
+});
